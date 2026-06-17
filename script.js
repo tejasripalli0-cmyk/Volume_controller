75c956960aae7c8e7431299a957389a215b77c91
tejasripalli0-cmyk <tejasripalli0@gmail.com>
2026-06-18 00:30:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const slider = document.getElementById("volumeSlider");
 const volumeValue = document.getElementById("volumeValue");
+const audio = document.getElementById("audio");
 
 slider.addEventListener("input", () => {
+
     volumeValue.textContent =
         `Volume: ${slider.value}%`;
+
+    audio.volume = slider.value / 100;
 });
